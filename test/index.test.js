a29caf1b13630d7ab425bd2a88bf5dead399d934
KyeongSeopHam <rudtjq5724@gmail.com>
2023-11-04 04:32:20 +0900

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const factorial =  require('../src');
+
+describe('Factorial Function', () =>{
+
+it("correctly computes factiorial of a positive integer number", () =>{
+
+    expect(factorial(3)).toEqual(6);
+})
+
+
+it('throws an error if a negative input is provided' , () => {
+
+    expect(()=>{
+        factorial(-1);
+    }).toThrow('Factorial is only defined for non-negative integers!');
+});
+
+});
